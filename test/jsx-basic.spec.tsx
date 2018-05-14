981a48b0a5a01444041b1f59d8b475c94c25c4ba
Manish Vachharajani <manishv@unbounded.systems>
2018-05-14 11:36:53 -0600

    Basic tests for SFC Components.

--- test/jsx-basic.spec.tsx
+++ test/jsx-basic.spec.tsx
@@ -28,7 +28,54 @@ function hasChildren(x: any): x is { children: unbs.UnbsNode[] } {
     return x.children != null;
 }
 
-describe('JSX createElement Tests', () => {
+
+describe('JSX SFC createElement Tests', () => {
+    function Component(props: any): unbs.UnbsNode {
+        throw new Error("Test is not supposed to render");
+    }
+
+    it('Element should construct', () => {
+        <Component />
+    });
+
+    it('Should have the correct type', () => {
+        const element = <Component />;
+        should(unbs.isNode(element)).True();
+    });
+
+    it('Should have the correct componentType', () => {
+        const element = <Component />;
+        should(element.componentType).equal(Component);
+    });
+
+    it('Should have the right props', () => {
+        const element = <Component x={1} y="bar" />;
+        should(element.props).eql({ x: 1, y: "bar" });
+    });
+
+    it('Should have the right children', () => {
+        const element =
+            <Component>
+                <Dummy />
+                <unbs.Group />
+            </Component>
+        
+        if (hasChildren(element.props)) {
+            should(element.props.children).not.be.Null();
+            should(element.props.children).be.Array();
+            should(element.props.children.length).equal(2);
+
+            const childComponents = element.props.children.map(
+                (child) => child.componentType);
+            should(childComponents).eql([Dummy, unbs.Group]);
+        } else {
+            throw new Error("Element does not have children: "
+                + util.inspect(element));
+        }
+    });
+});
+
+describe('JSX Class createElement Tests', () => {
     it('Element should construct', async () => {
         <unbs.Group />
     });
@@ -59,7 +106,6 @@ describe('JSX createElement Tests', () => {
             should(element.props.children).not.be.Null();
             should(element.props.children).be.Array();
             should(element.props.children.length).equal(2);
-
 
             const childComponents = element.props.children.map(
                 (child) => child.componentType);
