ccd4045907ec7fdbe5a2fd78202770d3059c215c
Manish Vachharajani <manishv@unbounded.systems>
2018-05-18 21:40:01 -0600

    Descendant selector code, tests and debug next.

--- src/css.ts
+++ src/css.ts
@@ -27,7 +27,8 @@ interface matchConfigType {
 
 const matchConfig: matchConfigType = {
     "tag": matchTag,
-    "child": matchChild
+    "child": matchChild,
+    "descendant": () => { throw new Error("Internal Error: should not get here"); }
 }
 
 function last<T>(arr: T[]): [T[], T | null] {
@@ -83,7 +84,30 @@ function matchWithSelector(
     path: jsx.UnbsElement[]): boolean {
 
     for (const block of selector) {
-        if(matchBlock(block, path)) {
+        if (matchBlock(block, path)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function matchDescendant(
+    selector: cssWhat.ParsedSelectorBlock,
+    path: jsx.UnbsElement[]): boolean {
+
+    if (selector.length <= 0) {
+        throw new Error("Internal Error: validated but malformed CSS" +
+            util.inspect(selector));
+    }
+
+    //Note(manishv) An optimization here is to find the deepest element in path 
+    //that matches the next set of selectors up to the next descendant selector
+    //and use that path up to that node as tryPath.  If it failse, 
+    //use the next deepest, etc.  Not sure that saves much though because that is
+    //what happens already, albiet through several function calls.
+    for (let i = 1; i < path.length - 1; i++) {
+        const tryPath = path.slice(0, -i);
+        if (matchWithSelector([selector], tryPath)) {
             return true;
         }
     }
@@ -98,14 +122,24 @@ function matchBlock(
     if (selFrag == null) {
         return true; //Empty selector matches everything
     }
-    const [newPath, fragResult] = matchFrag(selFrag, path);
-    if (!fragResult) return false;
 
-    return matchBlock(prefix, newPath);
+    if (selFrag.type == "descendant") {
+        return matchDescendant(prefix, path);
+    } else {
+        const [newPath, fragResult] = matchFrag(selFrag, path);
+        if (!fragResult) return false;
+
+        return matchBlock(prefix, newPath);
+    }
+}
+
+function validateSelector(selector: cssWhat.ParsedSelector) {
+    return; //FIXME(manishv) Actuall validate CSS parse tree here
 }
 
 function buildStyle(rawStyle: RawStyle): Style {
     const selector = cssWhat(rawStyle.selector, { xmlMode: true });
+    validateSelector(selector);
     return {
         match: (path: jsx.UnbsElement[]) =>
             matchWithSelector(selector, path),
