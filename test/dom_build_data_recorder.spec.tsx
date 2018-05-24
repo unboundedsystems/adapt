edbfd5afe23b82621135aac08d70c6035715ede0
Manish Vachharajani <manishv@unbounded.systems>
2018-05-24 16:06:23 -0600

    step, elementBuilt test

--- test/dom_build_data_recorder.spec.tsx
+++ test/dom_build_data_recorder.spec.tsx
@@ -27,4 +27,20 @@ describe("Build Data Recorder", () => {
         should(record[0]).eql({ type: "start", root: dom });
     });
 
+    it("should record step, elementBuilt", () => {
+        const dom = <Empty id={1} />;
+        const newElem = unbsBuild(dom, null, { recorder });
+        const record1 = record[1];
+        const record2 = record[2];
+        if (record1.type === "step") {
+            should(record1).eql({ type: "step", oldElem: dom, newElem, style: undefined });
+            should(record2).eql({
+                type: "elementBuilt",
+                oldElem: dom,
+                newElem: record1.newElem
+            });
+        } else {
+            should(record1.type).equal("step");
+        }
+    });
 });
