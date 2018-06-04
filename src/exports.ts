0aa23e806c02a6a5dd7e3373da53adabae813d68
Mark Terrel <mark@unbounded.systems>
2018-06-04 12:22:05 -0600

    Add default export to unbs to enable single import statement usage.

---
+++ src/exports.ts
@@ -0,0 +1,31 @@
+export * from "./jsx_namespace";
+export {
+    createElement,
+    cloneElement,
+    Component,
+    PrimitiveComponent,
+    UnbsElement,
+    UnbsNode,
+    AnyProps,
+    isElement,
+    isPrimitiveElement
+} from "./jsx";
+
+export {
+    Group
+} from "./builtin_components";
+
+export {
+    build
+} from "./dom";
+
+export {
+    Style,
+    rule
+} from "./css";
+
+export {
+    serializeDom,
+} from "./dom_serialize";
+
+export * from "./dom_build_data_recorder";
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,3 @@
-export * from "./jsx_namespace";
-export {
-    createElement,
-    cloneElement,
-    Component,
-    PrimitiveComponent,
-    UnbsElement,
-    AnyProps,
-    isElement,
-    isPrimitiveElement
-} from "./jsx";
-
-export {
-    Group
-} from "./builtin_components";
-
-export {
-    build
-} from "./dom";
-
-export {
-    Style,
-    rule
-} from "./css";
-
-export {
-    serializeDom,
-} from "./dom_serialize";
-
-export * from "./dom_build_data_recorder";
+import * as unbs from "./exports";
+export default unbs;
+export * from "./exports";
