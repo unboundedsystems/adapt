60018aae268495e6006c9262372566cfd63dbc83
Manish Vachharajani <manishv@unbounded.systems>
2018-07-11 14:31:18 -0600

    Basic state object and tests.

--- adapt/src/state.ts
+++ adapt/src/state.ts
@@ -1,6 +1,7 @@
 import { AnyState } from "./jsx";
 
 export interface State {
+    setElementState(elem: BuildPath, data: any): void;
     elementState(elem: BuildPath): AnyState | null;
 }
 
@@ -12,6 +13,10 @@ export type BuildPath = string[];
 
 class StateImpl implements State {
     states = new Map<string, AnyState>();
+
+    setElementState(elem: BuildPath, data: any) {
+        this.states.set(JSON.stringify(elem), data);
+    }
 
     elementState(elem: BuildPath): AnyState | null {
         const ret = this.states.get(JSON.stringify(elem));
