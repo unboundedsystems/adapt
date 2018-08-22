a37714b4bf7048222d17256f439101081f48c145
Mark Terrel <mark@unbounded.systems>
2018-08-22 16:06:39 -0600

    testutils: Add more general k8s resource functions * getAll and deleteAll take an apiName (pods, services) param * check for adaptName in metadata.annotations in getAll and deleteAll

--- testutils/src/k8sutils.ts
+++ testutils/src/k8sutils.ts
@@ -1,4 +1,5 @@
 import { sleep } from "@usys/utils";
+import { filter } from "lodash";
 
 // tslint:disable-next-line:no-var-requires
 const k8s = require("kubernetes-client");
@@ -17,41 +18,60 @@ export async function getClient(config: KubeConfig): Promise<KubeClient> {
     return client;
 }
 
-export async function getPodsWithClient(client: KubeClient) {
-    if (client.api == null) throw new Error(`k8s client api is null`);
-    const pods = await client.api.v1.namespaces("default").pods.get();
-    if (pods.statusCode !== 200) {
-        throw new Error(`k8s client returned status ${pods.statusCode}`);
-    }
-    return pods.body.items;
+export function getK8sConfig(kubeConfig: KubeConfig): K8sConfig {
+    return k8s.config.fromKubeconfig(kubeConfig);
 }
+
+/*
+ * General resource utilities
+ */
+
+export async function getAllWithClient(apiName: string, client: KubeClient) {
+    if (client.api == null) throw new Error(`k8s client api is null`);
+    const response = await client.api.v1.namespaces("default")[apiName].get();
+    if (response.statusCode !== 200) {
+        throw new Error(`k8s client returned status ${response.statusCode}`);
+    }
+    const resources: any[] = response.body.items;
+    return filter(resources,
+                  (r) => r.metadata.annotations && r.metadata.annotations.adaptName);
+}
+
+export async function deleteAll(apiName: string, k8sConfig: K8sConfig, waitTimeMs = 15000) {
+    const client = await getClient(k8sConfig);
+    let resources = await getAllWithClient(apiName, client);
+    if (resources.length === 0) return;
+
+    for (const r of resources) {
+        await client.api.v1.namespaces("default")[apiName](r.metadata.name).delete();
+    }
+
+    do {
+        resources = await getAllWithClient(apiName, client);
+        if (resources.length === 0) return;
+        await sleep(1000);
+        waitTimeMs -= 1000;
+    } while (waitTimeMs > 0);
+
+    throw new Error(`Failed to remove ${apiName}: ${JSON.stringify(resources, null, 2)}`);
+}
+
+export async function getAll(apiName: string, config: KubeConfig) {
+    const client = await getClient(config);
+    return getAllWithClient(apiName, client);
+}
+
+/*
+ * Legacy: Pods
+ */
 
 export async function getPods(config: KubeConfig) {
     const client = await getClient(config);
     return getPodsWithClient(client);
 }
-
-export function getK8sConfig(kubeConfig: KubeConfig): K8sConfig {
-    return k8s.config.fromKubeconfig(kubeConfig);
+export async function getPodsWithClient(client: KubeClient) {
+    return getAllWithClient("pods", client);
 }
-
 export async function deleteAllPods(k8sConfig: K8sConfig) {
-    const client = await getClient(k8sConfig);
-    let pods = await getPodsWithClient(client);
-
-    for (const pod of pods) {
-        await client.api.v1.namespaces("default").pods(pod.metadata.name).delete();
-    }
-
-    const retries = 3;
-    let count = 0;
-    do {
-        pods = await getPodsWithClient(client);
-        await sleep(5000);
-        count++;
-    } while (pods.length !== 0 && count < retries);
-
-    if (pods.length !== 0) {
-        throw new Error(`Failed to remove pods: ${JSON.stringify(pods, null, 2)}`);
-    }
+    return deleteAll("pods", k8sConfig);
 }
