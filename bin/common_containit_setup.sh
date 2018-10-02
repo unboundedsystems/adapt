72a2c86285256dad4356063d04488c185b42c84b
Mark Terrel <mark@unbounded.systems>
2018-10-01 19:23:52 -0600

    DOCKER_ARGS in bin/registry out of sync with bin/node Pull common containit (and containit-like) setup into a single file where multiple different shell scripts can use it.

---
+++ bin/common_containit_setup.sh
@@ -0,0 +1,33 @@
+# Common containit script environment setup
+#
+# Must set TOP_DIR prior to sourcing this script.
+
+CTR_ADD_PATH="/src/node_modules/.bin"
+
+if [[ $(pwd) =~ ${TOP_DIR}/([^/]+) ]]; then
+    PROJECT="${BASH_REMATCH[1]}"
+    if [ -n "${PROJECT}" ]; then
+        CTR_ADD_PATH="/src/${PROJECT}/node_modules/.bin:${CTR_ADD_PATH}"
+    fi
+fi
+
+# Ensure the DNS cache is running and use that as the DNS server
+DNS_IP=$("${TOP_DIR}/bin/dnsmasq")
+if [ $? -ne 0 ]; then
+    echo "Error starting DNS cache"
+    exit 1
+fi
+DOCKER_ARGS+=" --dns ${DNS_IP}"
+
+DOCKER_ARGS+=" -eYARN_CACHE_FOLDER=/root/.cache/yarn -v${HOME}/.cache/yarn:/root/.cache/yarn"
+DOCKER_ARGS+=" -v/var/run/docker.sock:/var/run/docker.sock"
+
+CRED_FILE="${HOME}/.adaptAwsCreds"
+if [ -f "${CRED_FILE}" ]; then
+    DOCKER_ARGS+=" -v${CRED_FILE}:/root/.adaptAwsCreds"
+fi
+
+# Propagate these from current environment into the docker container env
+DOCKER_ARGS+=" -eADAPT_PARALLEL_MAKE"
+DOCKER_ARGS+=" -eADAPT_TEST_MINIKUBE -eADAPT_RUN_LONG_TESTS"
+DOCKER_ARGS+=" -eAWS_ACCESS_KEY_ID -eAWS_SECRET_ACCESS_KEY -eAWS_DEFAULT_REGION"
