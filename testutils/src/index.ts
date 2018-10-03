8f1feb5f53a77e918510a3b3e8f772c9aef950ca
Mark Terrel <mark@unbounded.systems>
2018-10-02 23:52:37 -0600

    testutils: Added heapdump-mocha Memory leak testing utility for mocha. Enable with ADAPT_TEST_HEAPDUMP=1

--- testutils/src/index.ts
+++ testutils/src/index.ts
@@ -3,12 +3,14 @@ export * from "./long_tests";
 export * from "./mocklogger";
 
 import * as awsutils from "./awsutils";
+import * as heapdumpMocha from "./heapdump-mocha";
 import * as k8sutils from "./k8sutils";
 import * as minikube from "./minikube";
 import * as minikubeMocha from "./minikube-mocha";
 
 export {
     awsutils,
+    heapdumpMocha,
     k8sutils,
     minikube,
     minikubeMocha,
