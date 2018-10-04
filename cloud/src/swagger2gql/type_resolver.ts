50bc57dfdd4d352fbc4e906072bf9ef51c29b5a9
Manish Vachharajani <manishv@unbounded.systems>
2018-10-04 13:24:44 -0600

    Add the ability to reject individual type resolution errors to TypeResolver

--- cloud/src/swagger2gql/type_resolver.ts
+++ cloud/src/swagger2gql/type_resolver.ts
@@ -1,11 +1,17 @@
+import { EventEmitter } from "events";
+import { isError } from "lodash";
+
 interface PendingType<Type> {
     res: (ty: Type) => void;
     rej: (e: Error) => void;
 }
 
-export class TypeResolver<Type> {
-    resolvedTypes = new Map<string, Type>();
+export class TypeResolver<Type> extends EventEmitter {
+    resolvedTypes = new Map<string, Type | Error>();
     pendingTypes = new Map<string, PendingType<Type>>();
+
+    addListener: (event: "needType", l: (resolver: this, name: string) => void) => this = super.addListener;
+    emit: (event: "needType", resolver: this, name: string) => boolean = super.emit;
 
     addType = (tyName: string, ty: Type) => {
         if (this.resolvedTypes.has(tyName)) throw new Error(`Attempt to add duplicate type '{$tyName}'`);
@@ -20,8 +26,13 @@ export class TypeResolver<Type> {
         function noop() { return; }
 
         const ty = this.resolvedTypes.get(tyName);
-        if (ty !== undefined) return ty;
+        if (ty !== undefined) {
+            if (isError(ty)) throw ty;
+            return ty;
+        }
+
         const pending = this.pendingTypes.get(tyName);
+        const firstTime = pending === undefined;
         const pres = pending ? pending.res : noop;
         const prej = pending ? pending.rej : noop;
         return new Promise<Type>((res, rej) => {
@@ -30,11 +41,22 @@ export class TypeResolver<Type> {
                 rej: (e: Error) => { prej(e); rej(e); }
             };
             this.pendingTypes.set(tyName, newPending);
+            if (firstTime) {
+                this.emit("needType", this, tyName);
+            }
         });
     }
 
     rejectPending = () => {
         this.pendingTypes.forEach((p, n) => p.rej(new Error(`Unable to resolve type '${n}'`)));
         this.pendingTypes.clear();
+    }
+
+    resolveError = (tyName: string, e: Error) => {
+        const pending = this.pendingTypes.get(tyName);
+        if (pending === undefined) throw new Error(`Cannot have a resolve error for not-pending type: ${tyName}`);
+        this.resolvedTypes.set(tyName, e);
+        this.pendingTypes.delete(tyName);
+        pending.rej(e);
     }
 }
--- cloud/test/swagger2gql/type_resolver.spec.ts
+++ cloud/test/swagger2gql/type_resolver.spec.ts
@@ -38,7 +38,7 @@ describe("TypeResolver Tests", () => {
         should(await res.getType("string")).eql(GraphQLString);
     });
 
-    it("Should reject pending types with error", async () => {
+    it("Should reject all pending types with error", async () => {
         const stringTyP = res.getType("string");
         const numberTyP = res.getType("number");
         should(stringTyP).Promise();
@@ -50,6 +50,20 @@ describe("TypeResolver Tests", () => {
         res.rejectPending();
         await should(numberTyP).rejectedWith(Error);
 
+        should(await res.getType("string")).eql(GraphQLString);
+    });
+
+    it("Should reject pending types when resolution fails", async () => {
+        const stringTyP = res.getType("string");
+        const numberTyP = res.getType("number");
+        should(stringTyP).Promise();
+        should(numberTyP).Promise();
+
+        res.resolveError("number", new Error("Whoops"));
+        await should(numberTyP).rejectedWith("Whoops");
+        await should(res.getType("number")).rejectedWith("Whoops");
+
+        res.addType("string", GraphQLString);
         should(await res.getType("string")).eql(GraphQLString);
     });
 
