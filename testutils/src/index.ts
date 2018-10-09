05de07cde0a5ed4805bfdaf5f58a30aac56cf628
Mark Terrel <mark@unbounded.systems>
2018-10-09 12:04:34 -0600

    testutils: Add docker-mocha docker-mocha is a test fixture to easily run a container alongside your mocha tests. * Pulled a bunch of non-minikube-specific code from minikube.ts into   dockerutils.ts. * Added signal handling to docker-mocha so it cleans itself up on Ctrl-C   and other signals. Changed to using node-graceful for signal handling   everywhere because it supports async code execution.

--- testutils/src/index.ts
+++ testutils/src/index.ts
@@ -2,6 +2,7 @@ export * from "./long_tests";
 export * from "./mocklogger";
 
 import * as awsutils from "./awsutils";
+import * as dockerMocha from "./docker-mocha";
 import * as heapdumpMocha from "./heapdump-mocha";
 import * as k8sutils from "./k8sutils";
 import * as minikube from "./minikube";
@@ -9,6 +10,7 @@ import * as minikubeMocha from "./minikube-mocha";
 
 export {
     awsutils,
+    dockerMocha,
     heapdumpMocha,
     k8sutils,
     minikube,
--- utils/src/mkdtmp.ts
+++ utils/src/mkdtmp.ts
@@ -1,14 +1,12 @@
 import * as fs from "fs-extra";
+import graceful from "node-graceful";
 import * as os from "os";
 import * as path from "path";
-
-// tslint:disable-next-line:no-var-requires
-const onExit = require("signal-exit");
 
 export async function mkdtmp(prefix: string): Promise<string> {
     return fs.mkdtemp(path.join(os.tmpdir(), prefix + "-"))
         .then((newDir) => {
-            onExit(() => fs.removeSync(newDir));
+            graceful.on("exit", () => fs.removeSync(newDir), true);
             return newDir;
         });
 }
