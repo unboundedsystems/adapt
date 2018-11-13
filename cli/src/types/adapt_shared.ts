8d21cab78e6404d36b63efb33a351179504e1179
Manish Vachharajani <manishv@unbounded.systems>
2018-11-13 11:43:51 -0700

    Add mountedOrigStatus to cli shared types.

--- cli/src/types/adapt_shared.ts
+++ cli/src/types/adapt_shared.ts
@@ -57,6 +57,7 @@ export interface DeploySuccess {
     stateJson: string;
     needsData: ObserversThatNeedData;
     deployID: string;
+    mountedOrigStatus: any;
 }
 
 export interface DeployError {
