717cbef564d4cab2f44bb6f727a96a90d460fc1e
Mark Terrel <mark@unbounded.systems>
2018-11-13 18:03:19 -0700

    testutils: bin/registry broken again Ensure each run of bin/registry gets its own storage for verdaccio

--- testutils/bin/run-local-registry.js
+++ testutils/bin/run-local-registry.js
@@ -1,8 +1,13 @@
 const defaults = require("../dist/src/local-registry-defaults");
 const registry = require("../dist/src/local-registry");
+const utils = require("@usys/utils");
 
 async function main() {
-    await registry.start(defaults.config, defaults.configPath);
+    const storage = await utils.mkdtmp("adapt-local-registry");
+    await registry.start({
+        ...defaults.config,
+        storage,
+    }, defaults.configPath);
 }
 
 main();
