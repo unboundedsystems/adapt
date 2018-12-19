54d8ddfceaa8ff116a6f4f2c3b5c7a43a254db8f
Manish Vachharajani <manishv@unbounded.systems>
2018-12-19 13:11:59 -0700

    Add support for destroying deployments.

--- cli/src/commands/deploy/destroy.ts
+++ cli/src/commands/deploy/destroy.ts
@@ -1,7 +1,11 @@
+import { dirname } from "path";
+import { isError } from "util";
 import { DeployOpBase } from "../../base/deploy_base";
+import { projectAdaptModule } from "../../proj";
+import { AdaptModule } from "../../types/adapt_shared";
 import { UpdateBaseCommand } from "./update";
 
-export default class StopCommand extends UpdateBaseCommand {
+export default class DestroyCommand extends UpdateBaseCommand {
     static description = "Stop an existing deployment of an Adapt project";
 
     static examples = [
@@ -26,6 +30,32 @@ Stop the deployment "myproj-dev-abcd" using the default project description file
 
     async run() {
         this.ctx.stackName = "(null)";
-        return super.run();
+        await this.addUpdateTask();
+        this.tasks.add({
+            title: "Destroying deployment",
+            task: async (_ctx, task) => {
+                let adapt: AdaptModule | undefined;
+                if (this.ctx.projectFile !== undefined) {
+                    try {
+                        adapt = await projectAdaptModule(dirname(this.ctx.projectFile));
+                    } catch (e) {
+                        task.title = "Destroying deployment (using internal adapt)";
+                        task.report(e);
+                    }
+                }
+                if (adapt === undefined) adapt = require("@usys/adapt");
+                if (adapt === undefined) throw new Error("Internal Error: adapt is undefined");
+
+                const result = await adapt.destroyDeployment({
+                    adaptUrl: this.ctx.adaptUrl,
+                    deployID: this.args.deployID,
+                    dryRun: this.ctx.dryRun,
+                    debug: this.ctx.debug
+                });
+
+                if (isError(result)) throw result;
+            }
+        });
+        await this.tasks.run();
     }
 }
