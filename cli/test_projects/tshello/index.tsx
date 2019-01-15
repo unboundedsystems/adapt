d10894cd739e5a96f366f347bd0545eb45546f4e
Manish Vachharajani <manishv@unbounded.systems>
2019-01-15 16:48:47 -0700

    Minor tweak to system test for use as a pitch demo

--- cli/test_projects/tshello/index.tsx
+++ cli/test_projects/tshello/index.tsx
@@ -1,8 +1,10 @@
 import Adapt, { Group } from "@usys/adapt";
 import TypeScriptService from "./TypeScriptService";
 
-const root = <Group>
-    <TypeScriptService srcDir="./code" port={8088} targetPort={8080} />
-</Group>;
+function App() {
+    return <Group>
+        <TypeScriptService srcDir="./code" port={8088} targetPort={8080} />
+    </Group>;
+}
 
-Adapt.stack("prod", root, null);
+Adapt.stack("prod", <App />, null);
