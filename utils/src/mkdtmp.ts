86ee45905a5ba33d6fb73464b812022a52ac0c75
Mark Terrel <mark@unbounded.systems>
2019-01-15 14:21:24 -0700

    utils: Add withTmpDir utility function

--- utils/src/mkdtmp.ts
+++ utils/src/mkdtmp.ts
@@ -3,10 +3,34 @@ import graceful from "node-graceful";
 import * as os from "os";
 import * as path from "path";
 
-export async function mkdtmp(prefix: string): Promise<string> {
-    return fs.mkdtemp(path.join(os.tmpdir(), prefix + "-"))
+export async function mkdtmp(prefix: string, basedir = os.tmpdir()): Promise<string> {
+    return fs.mkdtemp(path.join(basedir, prefix + "-"))
         .then((newDir) => {
             graceful.on("exit", () => fs.removeSync(newDir), true);
             return newDir;
         });
+}
+
+export interface WithTmpDirOpts {
+    prefix?: string;
+    basedir?: string;
+}
+
+const withTmpDirDefaults = {
+    prefix: "tmp",
+    basedir: os.tmpdir(),
+};
+
+export async function withTmpDir<T>(
+    f: (tmpDir: string) => Promise<T> | T,
+    options: WithTmpDirOpts = {}): Promise<T> {
+
+    const { basedir, prefix } = { ...withTmpDirDefaults, ...options };
+
+    const tmpDir = await mkdtmp(prefix, basedir);
+    try {
+        return await f(tmpDir);
+    } finally {
+        await fs.remove(tmpDir);
+    }
 }
