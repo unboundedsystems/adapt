b626b56c4f1fd6acc41db6c4d2fff128cf2d87e1
Mark Terrel <mark@unbounded.systems>
2019-01-23 15:06:30 -0700

    demo: Add a couple of scripts to make setting up a demo easier

---
+++ demo/nodecellar.sh
@@ -0,0 +1,19 @@
+DEMO_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+. "${DEMO_DIR}/minikube.sh"
+
+cp -R /src/cli/test_projects/nodecellar /tmp
+cd /tmp/nodecellar
+
+minikubeConnect || \
+    { error Error connecting to minikube; return 1; }
+
+cat ~/.kube/config | toJson > ./kubeconfig.json || \
+    { error Error getting kubeconfig; return 1; }
+
+
+echo
+echo adapt deploy:create --init k8s
+echo adapt deploy:create --init dev
+echo adapt deploy:create --init aws
+
