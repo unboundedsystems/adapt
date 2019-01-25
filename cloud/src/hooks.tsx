d709f15f7cf9a15a33dc8438085f8d68796faa0a
Manish Vachharajani <manishv@unbounded.systems>
2019-01-25 12:24:58 -0700

    Add useMethod hook

--- cloud/src/hooks.tsx
+++ cloud/src/hooks.tsx
@@ -1,7 +1,16 @@
-import { useState } from "@usys/adapt";
+import { Handle, useState } from "@usys/adapt";
 
 export function useAsync<T>(f: () => Promise<T> | T, initial: T): T {
     const [val, setVal] = useState(initial);
     setVal(f);
     return val;
+}
+
+export function useMethod<T>(hand: Handle, initial: T, method: string, ...args: any[]) {
+    return useAsync<T>(async () => {
+        if (hand.mountedOrig == null) throw new Error("Handle does not point to element");
+        const result = hand.mountedOrig.instance[method](...args);
+        if (result === undefined) return initial;
+        return result;
+    }, initial);
 }
--- cloud/test/hooks.spec.tsx
+++ cloud/test/hooks.spec.tsx
@@ -1,6 +1,6 @@
-import Adapt from "@usys/adapt";
+import Adapt, { BuiltinProps, useImperativeMethods } from "@usys/adapt";
 import should from "should";
-import { useAsync } from "../src/hooks";
+import { useAsync, useMethod } from "../src/hooks";
 
 describe("useAsync hook tests", () => {
     it("Should return default and computed value", async () => {
@@ -10,7 +10,37 @@ describe("useAsync hook tests", () => {
             return null;
         }
 
-        await Adapt.build(<Test/>, null);
+        await Adapt.build(<Test />, null);
         should(val).eql([3, 10]);
+    });
+});
+
+describe("useMethod hook tests", () => {
+    it("Should return default and computed value", async () => {
+        const val: number[] = [];
+        function Test(props: Partial<BuiltinProps>) {
+            useImperativeMethods(() => ({
+                getVal: () => 10
+            }));
+            val.push(useMethod(props.handle!, 3, "getVal"));
+            return null;
+        }
+
+        await Adapt.build(<Test />, null);
+        should(val).eql([3, 10]);
+    });
+
+    it("Should forward extra arguments", async () => {
+        const val: number[] = [];
+        function Test(props: Partial<BuiltinProps>) {
+            useImperativeMethods(() => ({
+                add: (x: number, y: number) => x + y
+            }));
+            val.push(useMethod(props.handle!, 5, "add", 6, 5));
+            return null;
+        }
+
+        await Adapt.build(<Test />, null);
+        should(val).eql([5, 11]);
     });
 });
