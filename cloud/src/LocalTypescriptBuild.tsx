2ea43bae90a04a2379188d7631402faab47e7cee
Manish Vachharajani <manishv@unbounded.systems>
2019-01-25 14:38:29 -0700

    Extract dockerbuild function to start new LocalDockerBuild component.

--- cloud/src/LocalTypescriptBuild.tsx
+++ cloud/src/LocalTypescriptBuild.tsx
@@ -6,7 +6,6 @@ import Adapt, {
     useState
 } from "@usys/adapt";
 import { mkdtmp } from "@usys/utils";
-import execa from "execa";
 import fs from "fs-extra";
 import path from "path";
 import { useAsync } from "./hooks";
@@ -19,74 +18,6 @@ async function withTmpDir<T>(prefix: string, f: (tmpDir: string) => Promise<T> |
     } finally {
         await fs.remove(tmpDir);
     }
-}
-
-interface DockerBuildOptions {
-    dockerHost?: string;
-    forceRm?: boolean;
-    imageName?: string;
-    imageTag?: string;
-    uniqueTag?: boolean;
-}
-
-const defaultDockerBuildOptions = {
-    forceRm: true,
-    uniqueTag: false,
-};
-
-export interface ImageInfo {
-    id: string;
-    nameTag?: string;
-}
-
-async function dockerBuild(dockerfile: string, contextPath: string,
-    options: DockerBuildOptions = {}): Promise<ImageInfo> {
-
-    const opts = { ...defaultDockerBuildOptions, ...options };
-    let nameTag: string | undefined;
-
-    const args = [ "build", "-f", dockerfile ];
-    if (opts.forceRm) args.push("--force-rm");
-    if (opts.dockerHost) args.push("-H", opts.dockerHost);
-    if (opts.imageName) {
-        const tag = createTag(opts.imageTag, opts.uniqueTag);
-        nameTag = tag ? `${opts.imageName}:${tag}` : opts.imageName;
-        args.push("-t", nameTag);
-    }
-    args.push(contextPath);
-
-    const { stdout, stderr } = await execa("docker", args);
-    const match = /^Successfully built ([0-9a-zA-Z]+)$/mg.exec(stdout);
-    if (!match || !match[1]) throw new Error("Could not extract image sha\n" + stdout + "\n\n" + stderr);
-    const inspectOut = await execa.stdout("docker", ["inspect", match[1]]);
-    try {
-        const inspect = JSON.parse(inspectOut);
-        if (!Array.isArray(inspect)) throw new Error(`Image inspect result is not an array`);
-        if (inspect.length === 0) throw new Error(`Built image ID not found`);
-        if (inspect.length > 1) throw new Error(`Multiple images found for ID`);
-
-        const ret: ImageInfo = { id: inspect[0].Id };
-        if (nameTag) ret.nameTag = nameTag;
-        return ret;
-
-    } catch (err) {
-        throw new Error(`Error while inspecting built image ID ${match[1]}: ${err.message}`);
-    }
-}
-
-function createTag(baseTag: string | undefined, appendUnique: boolean): string | undefined {
-    if (!baseTag && !appendUnique) return undefined;
-    let tag = baseTag || "";
-    if (baseTag && appendUnique) tag += "-";
-    if (appendUnique) {
-        tag += randomstring.generate({
-            length: 8,
-            charset: "alphabetic",
-            readable: true,
-            capitalization: "lowercase",
-        });
-    }
-    return tag;
 }
 
 export async function localTypescriptBuild(srcDir: string,
@@ -124,6 +55,7 @@ function LocalTypescriptBuild(props: TypescriptBuildProps) {
 
     useImperativeMethods(() => ({
         buildComplete: () => image !== undefined,
+        ready: () => image !== undefined,
         image
     }));
     return null;
@@ -164,4 +96,3 @@ export function useTypescriptBuild(srcDir: string,
 
     return { image: buildState, buildObj: null };
 }
-
