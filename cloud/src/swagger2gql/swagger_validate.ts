124237973155c420999b9a014a28ee3901b0343b
Mark Terrel <mark@unbounded.systems>
2019-01-30 23:36:57 -0700

    fix: Building DOM stage takes 30+ sec when deploying The issue was due to swagger spec validation in swagger2gql. Hashes of the currently used swagger specs have been pre-populated into a list of known valid specs. TODO: Automatically update the hashes or write a unit test that fails if full validation is done on any files we package.

--- cloud/src/swagger2gql/swagger_validate.ts
+++ cloud/src/swagger2gql/swagger_validate.ts
@@ -1,3 +1,4 @@
+import db from "debug";
 import jsonStableStringify = require("json-stable-stringify");
 import { Validator } from "jsonschema";
 import { Swagger2 } from "./swagger_types";
@@ -6,30 +7,41 @@ import { createHash } from "crypto";
 import jsonSchemaSchema = require("./json-schema-4.json");
 import swagger2schema = require("./swagger2schema.json");
 
-let validator: Validator;
-const validMap = new Map<string, boolean>();
+const debug = db("adapt:cloud:swagger2gql");
 
-function validMapKey(cand: unknown): string {
+const knownSwaggerSpecs = [
+    "3f9280af09efeaf35940feb34b7449de", // AwsCfObserver
+    "7cf922b28a4f6562069fe68c716bb6d7", // AwsEc2Observer
+    "1cf94afa6c91f4c71cb4c37182ecee6e", // DockerObserver
+    "5eb0190ff7afd4ef33bc9dec64a8ed04", // K8sObserver
+];
+
+let validator: Validator;
+const validSet = new Set<string>(knownSwaggerSpecs);
+
+function validSetKey(cand: unknown): string {
     const json = jsonStableStringify(cand);
-    return createHash("md5").update(json, "utf8").digest().toString();
+    return createHash("md5").update(json, "utf8").digest("hex");
 }
 
 function isValidated(cand: unknown): boolean {
-    const key = validMapKey(cand);
-    return validMap.get(key) || false;
+    const key = validSetKey(cand);
+    const found = validSet.has(key);
+    if (!found) debug(`Swagger spec with hash ${key} has not been pre-validated`);
+    return found;
 }
 
 function validated(cand: unknown): void {
-    const key = validMapKey(cand);
-    validMap.set(key, true);
+    const key = validSetKey(cand);
+    validSet.add(key);
 }
 
 export function validateSwagger2(cand: unknown): Swagger2 {
+    if (isValidated(cand)) return cand as Swagger2;
     if (!validator) {
         validator = new Validator();
         validator.addSchema(jsonSchemaSchema, "http://json-schema.org/draft-04/schema");
     }
-    if (isValidated(cand)) return cand as Swagger2;
     const result = validator.validate(cand, swagger2schema);
     if (result.errors.length > 0) {
         throw new Error("Invalid swagger specification: " + result.errors.join("\n"));
