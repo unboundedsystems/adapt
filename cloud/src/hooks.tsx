baf04cd0c12c9803a6d8abdd1c6345eb71a6f6e8
Manish Vachharajani <manishv@unbounded.systems>
2019-01-30 17:13:20 -0700

    Add utility functions to call methods and fetch values in an async context.

--- cloud/src/hooks.tsx
+++ cloud/src/hooks.tsx
@@ -1,4 +1,5 @@
-import { Handle, useState } from "@usys/adapt";
+import { Handle, serializeDom, useState } from "@usys/adapt";
+import ld from "lodash";
 
 export function useAsync<T>(f: () => Promise<T> | T, initial: T): T {
     const [val, setVal] = useState(initial);
@@ -6,11 +7,37 @@ export function useAsync<T>(f: () => Promise<T> | T, initial: T): T {
     return val;
 }
 
+export function callInstanceMethod<T = any>(hand: Handle, def: T, methodName: string, ...args: any[]): T {
+    const method = getInstanceValue<(...args: any[]) => T>(hand, () => def, methodName);
+    const mountedOrig = hand.mountedOrig;
+    if (!ld.isFunction(method)) {
+        throw new Error(`${methodName} exists but is not a function on handle instance:\n`
+            + ((mountedOrig != null) ? serializeDom(mountedOrig, false) : `mountedOrig is ${mountedOrig}`));
+    }
+    return method(...args);
+}
+
+export function getInstanceValue<T = any>(hand: Handle, def: T | undefined, field: string): T {
+    const mountedOrig = hand.mountedOrig;
+    if (!mountedOrig) {
+        if (def !== undefined) return def;
+        throw new Error(`Cannot get instance field ${field}: Handle does not point to mounted element`);
+    }
+    if (!mountedOrig.instance) {
+        throw new Error(`Element is mounted but instance is ${mountedOrig.instance}`);
+    }
+    if (!(field in mountedOrig.instance)) {
+        throw new Error(`${field} does not exist on handle instance:\n` + serializeDom(mountedOrig, false));
+    }
+    const val = mountedOrig.instance[field];
+    if (ld.isFunction(val)) {
+        return val.bind(mountedOrig.instance);
+    }
+    return val;
+}
+
 export function useMethod<T>(hand: Handle, initial: T, method: string, ...args: any[]) {
     return useAsync<T>(async () => {
-        if (hand.mountedOrig == null) throw new Error("Handle does not point to element");
-        const result = hand.mountedOrig.instance[method](...args);
-        if (result === undefined) return initial;
-        return result;
+       return callInstanceMethod<T>(hand, initial, method, ...args);
     }, initial);
 }
