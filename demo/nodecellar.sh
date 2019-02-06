cebf732208f8cc908320c627c98f633551c650f2
Mark Terrel <mark@unbounded.systems>
2019-02-06 15:02:08 -0700

    demos: Set DOCKER_HOST to minikube dind host Updated the minikube demo script to distinguish between the two Docker instances to support this.

--- demo/nodecellar.sh
+++ demo/nodecellar.sh
@@ -1,10 +1,11 @@
+DEMO_NAME=nodecellar
 DEMO_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 . "${DEMO_DIR}/minikube.sh"
 . "${DEMO_DIR}/commands.sh"
 
-cp -R /src/cli/test_projects/nodecellar /tmp
-cd /tmp/nodecellar
+cp -R "/src/cli/test_projects/${DEMO_NAME}" /tmp
+cd "/tmp/${DEMO_NAME}"
 
 minikubeConnect || \
     { error Error connecting to minikube; return 1; }
@@ -12,6 +13,7 @@ minikubeConnect || \
 cat ~/.kube/config | toJson > ./kubeconfig.json || \
     { error Error getting kubeconfig; return 1; }
 
+export DOCKER_HOST=$(minikubeDockerHost)
 
 echo
 echo adapt deploy:create --init k8s
--- demo/tshello.sh
+++ demo/tshello.sh
@@ -1,16 +1,19 @@
+DEMO_NAME=tshello
 DEMO_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 . "${DEMO_DIR}/minikube.sh"
 . "${DEMO_DIR}/commands.sh"
 
-cp -R /src/cli/test_projects/tshello /tmp
-cd /tmp/tshello
+cp -R "/src/cli/test_projects/${DEMO_NAME}" /tmp
+cd "/tmp/${DEMO_NAME}"
 
 minikubeConnect || \
     { error Error connecting to minikube; return 1; }
 
 cat ~/.kube/config | toJson > ./kubeconfig.json || \
     { error Error getting kubeconfig; return 1; }
+
+export DOCKER_HOST=$(minikubeDockerHost)
 
 echo
 echo adapt deploy:create --init prod
