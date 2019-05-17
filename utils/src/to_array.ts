2fffb5ee55633fa8a5555a199756e854c82ccba8
Mark Terrel <mark@unbounded.systems>
2019-05-17 02:01:44 -0600

    utils fix: Return type of toArray incorrect If the arg type is X | X[], toArray should return X[]

--- utils/src/to_array.ts
+++ utils/src/to_array.ts
@@ -1,1 +1,4 @@
-export const toArray = <T>(val: T | T[]): T[] => Array.isArray(val) ? val : [ val ];
+export type ToArray<T> = T extends any[] ? T : T[];
+
+export const toArray = <T>(val: T): ToArray<T> =>
+    Array.isArray(val) ? val as any : [ val ];
