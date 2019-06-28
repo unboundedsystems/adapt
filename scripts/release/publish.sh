22803fe309c63a6b5139055d1bce73b5328c5202
Mark Terrel <mark@unbounded.systems>
2019-06-28 15:41:10 -0600

    release: Add --dev and --debug flags to publish script

--- scripts/release/publish.sh
+++ scripts/release/publish.sh
@@ -5,20 +5,27 @@ REPO_ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )
 . "${REPO_ROOT}/scripts/release/release_utils.sh"
 
 function usage {
-    echo 
-    echo Publishes the next version of all packages.
-    echo
-    echo "Usage:"
-    echo "  $0 [ VERSION_TYPE ]"
-    echo
-    echo "  VERSION_TYPE:"
-    echo "      One of: major, minor, or patch"
-    echo "      For a release branch, the default VERSION_TYPE is patch."
-    echo "      For any other branch, VERSION_TYPE is ignored and always"
-    echo "      set to prerelease."
-    echo
-    echo "Example:"
-    echo "  $0 minor"
+    cat <<USAGE
+
+Publishes the next version of all packages.
+
+Usage:
+  $0 [ FLAGS ] [ VERSION_TYPE ]
+
+  VERSION_TYPE:
+      One of: major, minor, or patch
+      For a release branch, the default VERSION_TYPE is patch.
+      For any other branch, VERSION_TYPE is ignored and always
+      set to prerelease.
+
+  FLAGS:
+      --dev         Allow publishing from a non-release branch
+      -h | --help   Display help for command
+
+Example:
+  $0 minor
+
+USAGE
 }
 
 function publishTagArgs {
@@ -56,26 +63,61 @@ function versionType {
     esac
 }
 
-if [[ $# -ne 0 && $# -ne 1 ]]; then
-    error ERROR: Incorrect number of arguments
-    usage
+LERNA_ARGS="publish"
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+
+        --debug)
+            LERNA_ARGS+=" --loglevel=debug"
+            ;;
+
+        --dev)
+            PUBLISH_DEV=1
+            ;;
+
+        -*)
+            error ERROR: Unrecognized flag: $1
+            usage
+            exit 1
+            ;;
+
+        *)
+            if [[ -n "${VERSION_ARG}" ]]; then
+                error ERROR: Too many arguments
+                usage
+                exit 1
+            fi
+
+            VERSION_ARG="$1"
+            ;;
+    esac
+    shift
+done
+
+
+LERNA_ARGS+=" $(preidArgs) $(publishTagArgs)"
+
+if [[ $PUBLISH_DEV -eq 1 ]] ; then
+    if $(isReleaseBranch) ; then
+        error "ERROR: Do not use --dev while on a release branch"
+        exit 1
+    fi
+    LERNA_ARGS+=" --allow-branch=$(currentBranch)"
+
+elif $(isReleaseBranch) ; then
+    :
+else
+    error ERROR: Publishing not permitted from branch $(currentBranch)
     exit 1
 fi
 
-case "$1" in
-    -h|--help)
-        usage
-        exit 0
-        ;;
-esac
+VERSION_TYPE=$(versionType "${VERSION_ARG}") || exit 1
+LERNA_ARGS+=" ${VERSION_TYPE}"
 
-if ! $(isReleaseBranch) ; then
-    echo ERROR: Publishing not permitted from branch $(currentBranch)
-    exit 1
-fi
-
-VERSION_TYPE=$(versionType "$1") || exit 1
-
-LERNA_ARGS="publish $(preidArgs) $(publishTagArgs) ${VERSION_TYPE}"
 echo "Running: lerna ${LERNA_ARGS}"
 "${REPO_ROOT}/node_modules/.bin/lerna" ${LERNA_ARGS}
