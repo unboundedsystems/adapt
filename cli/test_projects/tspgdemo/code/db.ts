5d5a808f6dd0fda49a1ecf02b5221748d446426f
Manish Vachharajani <manishv@unbounded.systems>
2019-07-30 11:54:57 -0600

    Remove tspgdemo, it is no longer used.

--- cli/test_projects/tspgdemo/code/db.ts
+++
@@ -1,7 +0,0 @@
-import { Pool } from "pg";
-
-const pool = new Pool();
-export async function getFirstMovieFromDB() {
-    const result = await pool.query("SELECT title FROM movies LIMIT 1");
-    return result.rows[0].title;
-}
--- cli/test_projects/tspgdemo/code/index.ts
+++
@@ -1,9 +0,0 @@
-import * as http from "http";
-import { getFirstMovieFromDB } from "./db";
-
-const srv = http.createServer((rq, rs) => {
-    getFirstMovieFromDB()
-        .then((name) => rs.end(`Hello World! The first movie is "${name}"!`))
-        .catch((e) => { rs.writeHead(500); rs.end(e.message); });
-});
-srv.listen(8080);
