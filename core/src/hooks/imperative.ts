4a0f74c918daa1c228bbe7681138523143c2160e
Mark Terrel <mark@unbounded.systems>
2019-08-01 13:32:13 -0600

    core: Allow type checking of useImperativeMethods argument

--- core/src/hooks/imperative.ts
+++ core/src/hooks/imperative.ts
@@ -2,7 +2,11 @@ import { InternalError } from "../error";
 import { GenericInstance, isElementImpl } from "../jsx";
 import { currentContext } from "./hooks";
 
-export function useImperativeMethods(create: () => GenericInstance) {
+export interface AnyMethods {
+    [ name: string ]: (...args: any[]) => any;
+}
+
+export function useImperativeMethods<T extends object = AnyMethods>(create: () => T & GenericInstance) {
     const ctx = currentContext();
 
     if (!isElementImpl(ctx.element)) throw new InternalError(`Build context element is not ElementImpl`);
