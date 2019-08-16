6bb661fbc989f7648d3bd37e806eae7d7351a338
Mark Terrel <mark@unbounded.systems>
2019-08-15 18:16:53 -0600

    Add copyright headers to source files

--- scripts/make_docs.js
+++ scripts/make_docs.js
@@ -1,5 +1,22 @@
 #!/usr/bin/env node
 
+/*
+ * Copyright 2019 Unbounded Systems, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 // This file is derived from:
 // https://github.com/faastjs/faast.js/blob/82057347adb0a1144dcf5615373c5e80ae2b9535/build/make-docs.js
 // Copyright 2019 Andy Chou
