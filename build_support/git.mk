543fbb014ca382eacc14e5a60b24abbd40f3262e
Mark Terrel <mark@unbounded.systems>
2019-08-19 16:14:34 -0600

    CI: Check for erroneous .git/hooks file

--- build_support/git.mk
+++ build_support/git.mk
@@ -2,7 +2,9 @@ GIT_HOOKS_SRC := $(wildcard build_support/git_hooks/*)
 GIT_HOOKS := $(patsubst build_support/git_hooks/%,.git/hooks/%,$(GIT_HOOKS_SRC))
 
 .git/hooks:
-	mkdir -p $@
+	if [ -f $@ ]; then rm -f $@ ; fi
+	if [ ! -d $@ ]; then mkdir -p $@ ; fi
+.PHONY: .git/hooks
 
 .git/hooks/%: build_support/git_hooks/% .git/hooks
 	cp $< $@
