488444328ae40706831656d6b5354e6af2fdc224
Mark Terrel <mark@unbounded.systems>
2019-11-04 23:59:07 -0700

    cloud: Add makeResourceName utility function

--- cloud/src/common.ts
+++ cloud/src/common.ts
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+import { sha256hex } from "@adpt/utils";
 import { ExecaError } from "execa";
 
 export function isExecaError(e: Error): e is ExecaError {
@@ -21,3 +22,45 @@ export function isExecaError(e: Error): e is ExecaError {
     if (!("exitCode" in e)) return false;
     return true;
 }
+
+/**
+ * Minimum number of hex characters for the SHA portion of a resource name.
+ * Keeping this value higher ensures lower probability of name collisions.
+ * @internal
+ */
+export const minShaLen = 8;
+/**
+ * Maximum number of hex characters for the SHA portion of a resource name.
+ * This value just keeps generated names from being annoyingly long to humans.
+ * @internal
+ */
+export const maxShaLen = 32;
+
+/**
+ * Creates a function for generating unique names for Adapt Elements.
+ * @remarks
+ * The unique names are unique by probability, based on a SHA256 hash.
+ * They are are made up of:
+ * - The Element's key, with any invalid characters removed.
+ * - A separator character (or string)
+ * - A portion of the SHA256 hash of the Element's ID and the deployID.
+ *
+ * @param - invalidChars - A regular expression that matches all invalid
+ * characters that should not be included in the resulting name. Must have
+ * the global flag set. Example:
+ * `/[^a-zA-Z-]/g`
+ * @param - maxLen - The maximum allowed lenth of name that should be generated.
+ * @param - sep - The separator string to be used between they key and the hash.
+ */
+export const makeResourceName = (invalidChars: RegExp, maxLen: number, sep = "-") => {
+    if (!invalidChars.global) {
+        throw new Error(`invalidChars must be a RegExp with the global flag set`);
+    }
+
+    return (elemKey: string, elemID: string, deployID: string) => {
+        const base = elemKey.toLowerCase().replace(invalidChars, "");
+        const baseLen = Math.min(maxLen - minShaLen - sep.length, base.length);
+        const shaLen = Math.min(maxShaLen, maxLen - baseLen - sep.length);
+        return base.slice(0, baseLen) + sep + sha256hex(elemID + deployID).slice(0, shaLen);
+    };
+};
