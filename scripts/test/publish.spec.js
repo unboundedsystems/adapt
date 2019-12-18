da6dedacf2d73b05331d009291d08067d67b32b9
Mark Terrel <mark@unbounded.systems>
2019-12-17 19:19:10 -0700

    scripts: Add git test fixture and git tests

--- scripts/test/publish.spec.js
+++ scripts/test/publish.spec.js
@@ -14,27 +14,42 @@
  * limitations under the License.
  */
 
-const { grep, repoRootDir }= require("@adpt/utils");
+const { grep } = require("@adpt/utils");
+const { mochaTmpdir } = require("@adpt/testutils");
 const db = require("debug");
 const execa = require("execa");
-const path = require("path");
+const fs = require("fs-extra");
+const { resolve, join } = require("path");
 const should = require("should");
+
+const { git } = require("./utils/git");
+const { useFixture } = require("./utils/git_fixture");
 
 const debug = db("adapt:test");
 
-const releaseDir = path.resolve(__dirname, "..", "release");
-const publishExe = path.join(releaseDir, "publish.sh");
+const releaseDir = resolve(__dirname, "..", "release");
+const publishExe = join(releaseDir, "publish.sh");
 
-async function publish(args, options = {}) {
-    const opts = { cwd: repoRootDir, ...options };
-    const subproc = execa(publishExe, args, opts);
+/**
+ * @param {any[] | readonly string[] | string[]} args
+ * @param {execa.Options<string>} [options]
+ */
+async function publish(args, options) {
+    const subproc = execa(publishExe, args, options);
     if (debug.enabled) {
         subproc.stdout.pipe(process.stdout);
         subproc.stderr.pipe(process.stderr);
     }
-    return subproc;
+    const ret = await subproc;
+    should(ret.stdout).not.match(/ERROR/);
+    should(ret.stderr).not.match(/ERROR/);
+    return ret;
 }
 
+/**
+ * @param {string[]} args
+ * @param {execa.Options<string>} [options]
+ */
 async function publishError(args, options = {}) {
     const opts = { all: true, ...options };
     try {
@@ -44,6 +59,9 @@ async function publishError(args, options = {}) {
     }
 }
 
+/**
+ * @param {string[]} argsIn
+ */
 async function getPublishCommand(argsIn) {
     const { stdout } = await publish(["--dry-run", ...argsIn]);
     const lines = await grep(stdout, /^\[SKIPPING\].*lerna publish/);
@@ -70,6 +88,12 @@ async function getPublishCommand(argsIn) {
 describe("Publish options", function() {
     this.timeout("20s");
 
+    mochaTmpdir.all("adapt-script-publish");
+
+    before(async () => {
+        await useFixture("simple");
+    });
+
     it("Should --debug enable lerna debug logging", async () => {
         const { args, options } = await getPublishCommand(["--debug", "--no-build", "prerelease"]);
         should(args).eql(["prerelease"]);
@@ -80,13 +104,13 @@ describe("Publish options", function() {
         this.timeout("5m");
         const { stdout } = await publish(["--dry-run", "prerelease"]);
         should(stdout).not.match(/SKIPPING.*make build/);
-        should(stdout).match(/cloud: docs COMPLETE/);
+        should(stdout).match(/^Build success/m);
     });
 
     it("Should not run 'make build' with --no-build", async () => {
         const { stdout } = await publish(["--dry-run", "--no-build", "prerelease"]);
         should(stdout).match(/SKIPPING.*make build/);
-        should(stdout).not.match(/cloud: docs COMPLETE/);
+        should(stdout).not.match(/^Build success/m);
     });
 
     it("Should not allow invalid dist-tags", async () => {
@@ -110,5 +134,83 @@ describe("Publish options", function() {
     it("Should require NPM registry to be set with --local", async () => {
         await should(publishError(["--no-build", "--dry-run", "--local", "prerelease"]))
             .be.rejectedWith(/NPM_CONFIG_REGISTRY must be set/);
+    });
+});
+
+describe("Publish branching", function() {
+    this.timeout("20s");
+
+    mochaTmpdir.all("adapt-script-publish");
+
+    before(async () => {
+        await useFixture("simple");
+        await git(["checkout", "-b", "other-branch"]);
+    });
+
+    it("Should only update git on release/master", async () => {
+        await git(["checkout", "master"]);
+        let ret = await publish(["--dry-run", "minor"]);
+        should(ret.stdout).match(/^Already up to date/m);
+
+        await git(["checkout", "release-1.0"]);
+        ret = await publish(["--dry-run", "patch"]);
+        should(ret.stdout).match(/^Already up to date/m);
+
+        await git(["checkout", "other-branch"]);
+        ret = await publish(["--dry-run", "dev"]);
+        should(ret.stdout).not.match(/^Already up to date/m);
+    });
+
+    it("Should only allow dev on non-release/master", async () => {
+        await git(["checkout", "other-branch"]);
+        const { args } = await getPublishCommand(["--dry-run", "dev"]);
+        should(args).eql(["prerelease"]);
+
+        await git(["checkout", "release-1.0"]);
+        await should(publishError(["--dry-run", "dev"]))
+            .be.rejectedWith(/Do not use 'dev' while on a release branch/);
+
+        await git(["checkout", "master"]);
+        await should(publishError(["--dry-run", "dev"]))
+            .be.rejectedWith(/Do not use 'dev' while on a release branch/);
+    });
+
+    it("Should only allow numbered release on master", async () => {
+        await git(["checkout", "master"]);
+        const { args } = await getPublishCommand(["--dist-tag", "beta", "2.0.0"]);
+        should(args).eql(["from-package"]);
+
+        await git(["checkout", "release-1.0"]);
+        await should(publishError(["--dry-run", "--dist-tag", "beta", "2.0.0"]))
+            .be.rejectedWith(/must be made from master/);
+
+        await git(["checkout", "other-branch"]);
+        await should(publishError(["--dry-run", "--dist-tag", "beta", "2.0.0"]))
+            .be.rejectedWith(/must be made from master/);
+    });
+
+    it("Should only allow minor release on master", async () => {
+        await git(["checkout", "master"]);
+        const { args } = await getPublishCommand(["--dist-tag", "beta", "minor"]);
+        should(args).eql(["minor"]);
+
+        await git(["checkout", "release-1.0"]);
+        await should(publishError(["--dry-run", "--dist-tag", "beta", "minor"]))
+            .be.rejectedWith(/must be made from master/);
+
+        await git(["checkout", "other-branch"]);
+        await should(publishError(["--dry-run", "--dist-tag", "beta", "minor"]))
+            .be.rejectedWith(/must be made from master/);
+    });
+
+    it("Should error with uncommitted changes", async () => {
+        try {
+            await git(["checkout", "master"]);
+            await fs.writeFile("help.txt", "some contents\n");
+            await should(publishError(["--dry-run", "prerelease"]))
+                .be.rejectedWith(/source tree must not have any modifications/);
+        } finally {
+            await fs.remove("help.txt");
+        }
     });
 });
