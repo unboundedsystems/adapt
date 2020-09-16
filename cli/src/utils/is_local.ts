c5eb4a67a2618500b4d3001f4f6f7d2636a29735
Mark Terrel <mark@unbounded.systems>
2020-09-16 01:11:58 -0600

    fix(utils): Fix EBUSY error on Windows during adapt run Issue was due to the isLocal utility function not correctly detecting a local path string on Windows.

--- cli/src/utils/is_local.ts
+++ cli/src/utils/is_local.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Unbounded Systems, LLC
+ * Copyright 2019-2020 Unbounded Systems, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+import path from "path";
+
 export function isLocal(filename: string) {
-    return (filename.startsWith(".") || filename.startsWith("/"));
+    return (filename.startsWith(".") || path.isAbsolute(filename));
 }
